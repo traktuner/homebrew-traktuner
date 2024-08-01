f1658346981665e7aef08403a822ef6baaa42d84
Traktuner <78533119+traktuner@users.noreply.github.com>
2024-08-01 07:19:51 +0200

    add microsoft-defender

---
+++ Casks/m/microsoft-defender.rb
@@ -0,0 +1,47 @@
+cask "microsoft-defender" do
+  version "101.24052.0013"
+  sha256 b5e8b9401c396da3d4f8ad56d3b358bb4ec91973b4614b8d6bf67243ae892797
+
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/wdav.pkg"
+  name "Microsoft Defender for Endpoint"
+  desc "Antivirus software"
+  homepage "https://www.microsoft.com/security/business/endpoint-security/microsoft-defender-endpoint"
+
+  livecheck do
+    url "https://aka.ms/MacDefender"
+    strategy :extract_plist
+  end
+
+  auto_updates true
+  depends_on cask: "microsoft-auto-update"
+  depends_on macos: ">= :big_sur"
+
+  pkg "wdav.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "com.microsoft.package.Microsoft_AutoUpdate.app",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  uninstall script:  {
+              executable: "/Library/Application\ Support/Microsoft/Defender/uninstall/uninstall",
+              sudo:       true,
+            },
+            pkgutil: [
+              "com.microsoft.dlp.agent",
+              "com.microsoft.dlp.daemon",
+              "com.microsoft.dlp.ux",
+            ],
+            quit:    "com.microsoft.autoupdate2"
+
+  zap trash: [
+    "~/Library/Group Containers/*com.microsoft.wdav/MicrosoftDefender.sqlite*",
+    "~/Library/Logs/Microsoft/Defender",
+  ]
+
+  postflight do
+    system_command "/bin/bash", args: ["-c", "~/Library/'Mobile\ Documents'/com~apple~CloudDocs/config/microsoft-defender/MicrosoftDefenderATPOnboardingMacOs.sh"], sudo: true
+  end
+end
