c7df5c04eee0629045c522aeaa7a37b57716abe6
Thomas <traktuner@pm.me>
2024-12-13 12:23:59 +0000

    network-share-mounter 3.1.0

--- Casks/n/network-share-mounter.rb
+++ Casks/n/network-share-mounter.rb
@@ -1,6 +1,6 @@
 cask "network-share-mounter" do
-  version "3.0.4"
-  sha256 "2f9f84f5e918630dbe0f149632a6f2d818c3ae22f22e3967fee01cbf6903bea9"
+  version "3.1.0"
+  sha256 "fcfc119f6cf09c4e1e66072f515c94c4118bc17a838191cb3cdc83cc9db1dff1"
 
   url "https://gitlab.rrze.fau.de/api/v4/projects/506/packages/generic/networksharemounter/release-#{version}/NetworkShareMounter-#{version}.dmg"
   name "Network Share Mounter"
@@ -10,15 +10,15 @@ cask "network-share-mounter" do
   livecheck do
     url "https://gitlab.rrze.fau.de/faumac/networkShareMounter/-/releases.json"
     regex(/"tag":"release-([0-9]+\.[0-9]+\.[0-9]+)"/i)
-  end  
+  end
 
   depends_on macos: ">= :big_sur"
-  
+
   app "Network Share Mounter.app"
 
   zap trash: [
     "~/Library/Application Scripts/de.fau.rrze.NetworkShareMounter-LaunchAtLoginHelper",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/de.fau.rrze.networksharemounter.sfl3",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/de.fau.rrze.networksharemounter.sfl*",
     "~/Library/Containers/de.fau.rrze.NetworkShareMounter-LaunchAtLoginHelper",
     "~/Library/HTTPStorages/de.fau.rrze.NetworkShareMounter",
     "~/Library/Preferences/de.fau.rrze.NetworkShareMounter.plist",
