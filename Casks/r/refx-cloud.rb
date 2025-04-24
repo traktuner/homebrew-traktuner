08959786e22718fe05b929ec448d3e90e09e68c9
Thomas <github.com.6iogk@passmail.com>
2025-04-24 08:33:13 +0200

    refx-cloud 3.0.17 (#12)

--- Casks/r/refx-cloud.rb
+++ Casks/r/refx-cloud.rb
@@ -1,15 +1,16 @@
 cask "refx-cloud" do
-  version "3.0.14"
-  sha256 "859e1e486d95b92cd4af34970466939e0466cb6bb244d133afe1cfdeadb1eea7"
+  version "3.0.17"
+  sha256 "18442a08234bb813515d0d06c004ca639cb755e03cdfb966d9023a4478dcc77e"
+
   url "https://cloud.refx.com/update/reFX_Cloud_#{version}.pkg"
   name "reFX Cloud Application"
   desc "Software to download reFX plugins and content"
-  homepage "https://refx.com"
+  homepage "https://refx.com/"
 
   livecheck do
     url "https://refx.com/cloud/version/"
     strategy :page_match
-    regex(/<div class="version mt-4">.*?<h1>(\d+\.\d+\.\d+)<\/h1>.*?<h2>\d{4}-\d{2}-\d{2}<\/h2>/i)
+    regex(%r{<div class="version mt-4">.*?<h1>(\d+\.\d+\.\d+)</h1>.*?<h2>\d{4}-\d{2}-\d{2}</h2>}i)
   end
 
   auto_updates true
@@ -17,14 +18,12 @@ cask "refx-cloud" do
 
   pkg "reFX_Cloud_#{version}.pkg"
 
-  uninstall quit: "com.refx.cloud",
-            pkgutil: [
-              "com.refx.pkg.reFXCloud"
-            ]
+  uninstall quit:    "com.refx.cloud",
+            pkgutil: "com.refx.pkg.reFXCloud"
 
   zap trash: [
     "~/Library/Application Support/reFX",
-    "~/Library/Saved Application State/com.refx.cloud.savedState",
     "~/Library/Caches/reFX Cloud",
+    "~/Library/Saved Application State/com.refx.cloud.savedState",
   ]
 end
