48e295da239d4e01c2aeb069c4640d49b54b95b6
Traktuner <traktuner@pm.me>
2025-08-11 06:49:43 +0200

    add 2fhey app

---
+++ Casks/2/2fhey.rb
@@ -0,0 +1,29 @@
+cask "2fhey" do
+  version "1.3"
+  sha256 :no_check
+
+  url "https://gumroad.com/r/e1621c37618aab38f0491222389c0595/product_files?product_file_ids%5B%5D=67GXU9CWO5B0AaTBx7cNqA%3D%3D"
+  name "2FHey"
+  desc "iMessage OTP/2FA AutoFill"
+  homepage "https://sofriendly.gumroad.com/l/2fhey"
+
+  livecheck do
+    url "https://gumroad.com/r/e1621c37618aab38f0491222389c0595/product_files?product_file_ids%5B%5D=67GXU9CWO5B0AaTBx7cNqA%3D%3D"
+    strategy :header_match
+    regex(/2FHey[_-]?(\d+\.\d+(?:\.\d+)?)\.dmg/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "2FHey.app"
+
+  uninstall quit: "com.sofriendly.2fhey"
+
+  zap trash: [
+    "~/Library/Application Support/2FHey",
+    "~/Library/Caches/com.sofriendly.2fhey",
+    "~/Library/Preferences/com.sofriendly.2fhey.plist",
+    "~/Library/Saved Application State/com.sofriendly.2fhey.savedState",
+  ]
+end
