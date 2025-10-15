3890c7f64ff1043b3db5db8d02114adce0892b65
Thomas <github.com.6iogk@passmail.com>
2025-10-15 20:59:05 +0200

    Update sha256 to no_check for 2fhey cask

--- Casks/2/2fhey.rb
+++ Casks/2/2fhey.rb
@@ -1,6 +1,6 @@
 cask "2fhey" do
   version "1.3"
-  sha256 "abdd1afbd8e9a02656ef7d09fdbff6698107676b29c451dbbad6fd20ca314654"
+  sha256 :no_check
 
   url "https://gumroad.com/r/8ead109659ab27fde77bb34e76c05b75/product_files?product_file_ids%5B%5D=SzwZXLb7jNWhYM4gmZiz7A%3D%3D"
   name "2FHey"
